123d2bd5391d43fd533a467966f4038c3d03c582
Fernando Obieta <fernando.obieta@blanktree.ch>
2019-05-15 10:35:51 +0200

    added the /css/ folder to git; moved the external js-components from the node_modules to /js/modules/*

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
 $(document).foundation();
+
+$(function() {
+	async function onPlay() {
+
+		// Settings
+		const SSD_MOBILENETV1 = 'ssd_mobilenetv1';
+
+
+		const videoEl = $('#inputVideo').get(0);
+
+		if(videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded())
+			return setTimeout(() => onPlay())
+
+
+
+
+	}
+});
