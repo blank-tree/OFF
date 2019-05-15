1896693cb38cddd76cf6cbf71582fdc77fb12910
Fernando Obieta <fernando.obieta@blanktree.ch>
2019-05-15 11:04:17 +0200

    basic face-api script. not working yet

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ $(function() {
 	async function onPlay() {
 
 		// Settings
-		const SSD_MOBILENETV1 = 'ssd_mobilenetv1';
+		let minConfidence = 0.5
 
 
 		const videoEl = $('#inputVideo').get(0);
@@ -12,8 +12,30 @@ $(function() {
 		if(videoEl.paused || videoEl.ended || !isFaceDetectionModelLoaded())
 			return setTimeout(() => onPlay())
 
+		const options = getFaceDetectorOptions();
 
+		const ts = Date.now();
 
+		const result = await faceapi.detectSingleFace(videoEl, options);
 
+		if (result) {
+			const canvas = $('#overlay').get(0);
+			const dims = faceapi.matchDimensions(canvas, videoEl, true);
+			faceapi.draw.drawDetections(canvas, faceapi.resizeResults(result, dims));
+		}
+
+		setTimeout(() => onPlay());
+	}
+
+	function getFaceDetectorOptions() {
+		return new faceapi.SsdMobilenetv1Options({ minConfidence });
+	}
+
+	function getCurrentFaceDetectionNet() {
+		return faceapi.nets.ssdMobilenetv1;
+	}
+
+	function isFaceDetectionModelLoaded() {
+		return !!getCurrentFaceDetectionNet().params
 	}
 });
