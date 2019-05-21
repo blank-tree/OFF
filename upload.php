d576095da2374aebf71fca5d949c107f5c449f1c
Fernando Obieta <fernando.obieta@blanktree.ch>
2019-05-21 10:51:27 +0200

    some cleanup

--- upload.php
+++ upload.php
@@ -1,8 +1,4 @@
 <?php
-
-// https://www.w3schools.com/php/php_file_upload.asp
-
-
 include 'secret.php';
 
 define('UPLOAD_DIR', 'uploads/');
@@ -41,80 +37,4 @@ if ($success) {
 	print 'Unable to save the file.';
 }
 
-
-/* // Version 1
-
-$current_timestamp = time();
-
-$target_dir = "uploads/";
-$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-
-$uploadOk = 1;
-$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-	$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-	if($check !== false) {
-		if ($DEBUG) echo "File is an image - " . $check["mime"] . ".";
-		$uploadOk = 1;
-	} else {
-		if ($DEBUG) echo "File is not an image.";
-		$uploadOk = 0;
-	}
-}
-// Check if file already exists
-if (file_exists($target_file)) {
-	if ($DEBUG) echo "Sorry, file already exists.";
-	$uploadOk = 0;
-}
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-	if ($DEBUG) echo "Sorry, your file is too large.";
-	$uploadOk = 0;
-}
-// Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-	&& $imageFileType != "gif" ) {
-	if ($DEBUG) echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
-$uploadOk = 0;
-}
-// Check if $uploadOk is set to 0 by an error
-if ($uploadOk == 0) {
-	if ($DEBUG) echo "Sorry, your file was not uploaded.";
-// if everything is ok, try to upload file
-} else {
-	if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        // echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
-
-
-        // https://www.w3schools.com/php/php_mysql_prepared_statements.asp
-
-        // Create connection
-		$conn = new mysqli($DB_URL, $DB_USER, $DB_PW, $DB_NAME);
-
-        // Check connection
-		if ($conn->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-		}
-
-        // prepare and bind
-		$stmt = $conn->prepare("INSERT INTO offuploads (filename, uploadtimestamp) VALUES (?, ?)");
-		$stmt->bind_param("ss", $filename, $uploadtimestamp);
-
-        // set parameters and execute
-		$filename = $target_file;
-		$uploadtimestamp = $current_timestamp;
-		$stmt->execute();
-
-		if ($DEBUG) echo "New upload created successfully";
-
-		$stmt->close();
-		$conn->close();
-
-
-
-	} else {
-		if ($DEBUG) echo "Sorry, there was an error uploading your file.";
-	}
-}
 ?>
