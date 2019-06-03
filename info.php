36a9f9614739812625d91ac834e3e08b6fd873f8
Fernando Obieta <fernando.obieta@blanktree.ch>
2019-06-03 14:19:23 +0200

    added info text and some small stylistic changes

--- info.php
+++ info.php
@@ -17,28 +17,33 @@
 
 		<div id="main-wrapper" class="grid-x grid-padding-x">
 
-			<div class="row">
+			<div class="row" id="info">
 				<div class="cell small-12" style="padding: 0 1rem 0 1rem">
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in risus in orci pellentesque suscipit. Aenean metus tellus, congue in odio non, mollis sagittis tortor. Phasellus dignissim, eros ac rhoncus auctor, quam metus ornare mi, ac euismod sem metus non enim. Sed sit amet ligula non ligula mattis commodo sit amet at risus. Nunc sit amet suscipit neque, sit amet dictum enim. Nunc tristique lectus sit amet neque commodo aliquet. Sed posuere diam sit amet varius interdum. Nulla vel nunc ut urna ultrices fringilla. Etiam orci libero, consequat in lectus non, feugiat sodales orci. Vestibulum sed tortor eget erat efficitur cursus.
-
-						Nullam elementum mollis lorem aliquam rhoncus. Vestibulum dolor metus, euismod vitae maximus non, ornare et nunc. In condimentum, nisi eu sollicitudin pharetra, tortor sem suscipit nisi, eu consequat quam turpis non est. Suspendisse pulvinar molestie sapien, sit amet sodales mauris semper nec. Morbi gravida orci ut orci dictum, vel suscipit purus interdum. Phasellus ac dolor sollicitudin, imperdiet turpis condimentum, scelerisque nunc. Fusce sagittis nulla bibendum, faucibus mauris sed, pellentesque magna. Nam efficitur ultrices lacinia. Praesent eget vehicula nibh. Vivamus sed nisi sit amet orci accumsan dictum. Donec fringilla volutpat lorem id efficitur. Aliquam lobortis rhoncus massa at consectetur.
-
-						Aliquam rutrum turpis vel velit consectetur venenatis. Nullam nec velit sit amet lectus gravida fermentum. Mauris pharetra, urna eget condimentum volutpat, metus tortor aliquam libero, id efficitur diam arcu sed dui. Suspendisse est lacus, fringilla ut dolor quis, luctus egestas diam. Integer justo sem, venenatis ut mauris at, rhoncus consequat nunc. Sed vulputate libero malesuada, tempor erat id, pellentesque velit. Nunc finibus ut quam et iaculis. Morbi eu massa id magna placerat lacinia.
-
-						Mauris aliquam nisl metus, ut facilisis dui aliquam at. Sed nunc sapien, consectetur vitae lectus quis, hendrerit dictum ante. Suspendisse potenti. Mauris fermentum diam quis condimentum luctus. Mauris laoreet, massa vitae tempor ornare, est enim euismod purus, quis lacinia felis risus vel justo. Fusce ac sapien ac sapien commodo accumsan non quis sapien. Maecenas in ipsum justo. Praesent molestie mi fermentum turpis scelerisque, nec ultrices libero dignissim. Nam est est, pulvinar non sagittis ut, posuere in arcu. Donec malesuada velit leo, sed hendrerit lectus gravida ultricies. Aenean sollicitudin leo tellus, ut efficitur diam ultricies eget. Donec interdum erat sem.
-
-						Suspendisse sem eros, maximus consectetur vestibulum ut, rutrum non tortor. Donec gravida tristique massa eget molestie. Integer velit ipsum, blandit nec sodales at, pulvinar quis nunc. Nulla nibh eros, dictum et tincidunt vel, fermentum sed leo. Praesent elementum mi sed finibus pharetra. Proin sed ultrices lacus. Suspendisse at luctus leo. Proin interdum, velit viverra pellentesque cursus, elit turpis tempor metus, quis vehicula purus elit non augue. Donec nec pharetra augue, in tristique nulla.
-
-						Quisque consequat tristique felis, in blandit tellus varius non. Nullam tristique lacinia vehicula. Donec sollicitudin venenatis augue. Morbi aliquam sapien eget fermentum pulvinar. Nam vitae aliquam est, rutrum venenatis lectus. Donec sed justo ac augue elementum lacinia. Maecenas non ultrices enim.
-
-						Mauris rhoncus at elit eu ultricies. Suspendisse potenti. Ut lectus lacus, rutrum sed commodo congue, venenatis vel risus. Vestibulum id leo sed felis gravida imperdiet interdum non quam. Phasellus mauris ligula, egestas et bibendum quis, sollicitudin at massa. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse potenti. Duis lobortis viverra tellus et maximus.
-
-						Nullam interdum consequat mattis. Sed aliquet vulputate elit. Cras suscipit nisl ac nunc pellentesque auctor. Cras porttitor ullamcorper augue sed porttitor. Phasellus ut velit feugiat, porttitor augue eu, bibendum est. Nunc in est egestas, tincidunt nunc sed, pellentesque nulla. Phasellus quam ante, rutrum eu euismod ut, egestas vel velit. Sed placerat odio vitae erat molestie finibus. Praesent interdum tellus dui, ut tincidunt tellus tincidunt id. Integer placerat tellus nulla, sit amet ullamcorper lorem suscipit ac.
-
-						Cras suscipit eros massa, id finibus felis suscipit eget. Praesent libero nunc, sagittis in tempor quis, fermentum sed urna. Donec tincidunt consequat eros, a commodo velit cursus ut. Aenean tempor purus sed ex vulputate, nec vestibulum magna consectetur. Ut suscipit nisi sed lectus tincidunt venenatis. In lacus arcu, gravida rhoncus lobortis id, aliquet eget mauris. Ut at luctus ipsum, vel rutrum magna. Curabitur ut imperdiet eros, et tincidunt ex. Phasellus ultricies diam at neque malesuada tincidunt. Fusce condimentum nibh id diam rhoncus, eu faucibus arcu tincidunt.
-
-						Nulla auctor odio eu ligula congue, non suscipit leo vestibulum. Sed non urna mauris. In mollis nec lectus at viverra. Donec id ligula id libero tristique ornare. In non dictum augue. Fusce risus justo, pharetra euismod risus non, imperdiet tempus mauris. Mauris pellentesque mollis quam, ut mollis diam elementum at. Mauris congue pellentesque orci, quis interdum turpis pretium at. Aliquam imperdiet ipsum at risus imperdiet pellentesque. Sed condimentum dictum metus, tempor placerat quam ullamcorper a. Nunc non rutrum nisl. Ut a lacus ex.
+						OFF ist die Entwicklung von Filtern, welche durch die präzise Platzierung auf dem menschlichen Gesicht eine Gesichtserkennung beinträchtigen.  Die Webseite dient als Werkzeug, um Gesichtsaufnahmen für den digitalen Gebrauch vorzubereiten. Durch die Anwendung der Filter kannst du selbst bestimmen, ob dein Gesicht auf deinen Posts, in deinen Stories und Chats erkannt wird, oder nicht.
+					</p>
+					<p>
+						Die Filter sind in folgende drei Kategorien eingeteilt:
+					</p>
+					<p>
+						<img src="static/img/recognized.svg" alt="Recognized" class="off-icon"> <span class="upper">Person Recognized</span>: Bist du ein Befürworter/eine Befürworterin der Gesichtserkennung und willst dem OFF-Trend folgen, ist die Anwendung der <img src="static/img/recognized.svg" alt="Recognized" class="off-icon"> Filter die richtige Entscheidung. Dein Gesicht wird erkannt und hilft dabei, die Datenbanken der Gesichtserkennung mit deinen Metadaten, wie Name, Beruf, Wohnort,… zu erweitern. 
+					</p>
+					<p>
+						<img src="static/img/detected.svg" alt="Detected" class="off-icon"> <span class="upper">Person Detected</span>: Du bist dafür, dass künstliche Intelligenzen neue Gesichter kennenlernen, möchtest aber deine Metadaten nicht in Verbindung mit deinem Gesicht weitergeben, dann nutze die <img src="static/img/detected.svg" alt="Detected" class="off-icon"> Filter. bezeichnet diejenigen Filter, die eine Person für die Software zwar erkennbar machen, jedoch keine Zuordnung der Metadaten ermöglichen. 
+					</p>
+					<p>
+						<img src="static/img/hidden.svg" alt="Hidden" class="off-icon"> <span class="upper">Person Hidden</span>: Die Gesichtserkennung hat deiner Meinung nach keine Vorteile und du möchtest dein Gesicht unerkennbar gestalten, dann sind es diese <img src="static/img/hidden.svg" alt="Hidden" class="off-icon"> Filter, welche du anwendest. Sie verunmöglichen die Erkennung eines Gesichts auf dem aufgenommenen Bild restlos.
+					</p>
+					<p>
+						Diese Webseite besitzt ein Gegenstück in Form eines gedruckten Readers. Dieser beinhaltet die insgesamt 544 verschiedenen Filter, sowie ausführliche Informationen zum Thema. Er bietet die Möglichkeit, sich auch ohne Zugang zum Internet zu informieren.
+					</p>
+					<p class="impressum">
+						Impressum: Bachelor Diplomarbeit 2019, ZHdK, Zürcher Hochschule der Künste<br>
+						Departement Design, Studienvertiefung Visuelle Kommunikation<br>
+						Autorin: Meret Fischli, Mentor/in: Cybu Richli, Martina Brassel<br>
+						© ZHdK Visuelle Kommunikation, Zürich, Juni 2019<br>
+						Interaction/Programmierung: Fernando Obieta<br>
+						Font: GT America Mono
 					</p>
 				</div>
 			</div>
