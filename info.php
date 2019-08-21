ae51c292a5a1d69b07d197f18e67f07dc680acf6
Fernando Obieta <fernando.obieta@blanktree.ch>
2019-08-21 10:23:35 +0200

    nice nice nice info page

--- info.php
+++ info.php
@@ -13,41 +13,52 @@
 	require_once('elements/header.php');
 	?>
 
-	<div id="main-container-archive" class="">
-
+	<div id="main-container-info" class="">
 		<div id="main-wrapper" class="grid-x grid-padding-x">
+			<div class="cell small-6">
+				<p>
+					OFF ist die Entwicklung von Filtern, welche durch die präzise Platzierung auf dem menschlichen Gesicht eine Gesichtserkennung beinträchtigen.  Die Webseite dient als Werkzeug, um Gesichtsaufnahmen für den digitalen Gebrauch vorzubereiten. Durch die Anwendung der Filter kannst du selbst bestimmen, ob dein Gesicht auf deinen Posts, in deinen Stories und Chats erkannt wird, oder nicht.
+				</p>
+				<p>
+					Diese Webseite besitzt ein Gegenstück in Form eines gedruckten Readers. Dieser beinhaltet die insgesamt 544 verschiedenen Filter, sowie ausführliche Informationen zum Thema. Er bietet die Möglichkeit, sich auch ohne Zugang zum Internet zu informieren.
+				</p>
+			</div>
+			
+		</div>
 
-			<div class="row" id="info">
-				<div class="cell small-12" style="padding: 0 1rem 0 1rem">
-					<p>
-						OFF ist die Entwicklung von Filtern, welche durch die präzise Platzierung auf dem menschlichen Gesicht eine Gesichtserkennung beinträchtigen.  Die Webseite dient als Werkzeug, um Gesichtsaufnahmen für den digitalen Gebrauch vorzubereiten. Durch die Anwendung der Filter kannst du selbst bestimmen, ob dein Gesicht auf deinen Posts, in deinen Stories und Chats erkannt wird, oder nicht.
-					</p>
-					<p>
-						Die Filter sind in folgende drei Kategorien eingeteilt:
-					</p>
-					<p>
-						<img src="static/img/recognized.svg" alt="Recognized" class="off-icon"> <span class="upper">Person Recognized</span>: Bist du ein Befürworter/eine Befürworterin der Gesichtserkennung und willst dem OFF-Trend folgen, ist die Anwendung der <img src="static/img/recognized.svg" alt="Recognized" class="off-icon"> Filter die richtige Entscheidung. Dein Gesicht wird erkannt und hilft dabei, die Datenbanken der Gesichtserkennung mit deinen Metadaten, wie Name, Beruf, Wohnort,… zu erweitern. 
-					</p>
-					<p>
-						<img src="static/img/detected.svg" alt="Detected" class="off-icon"> <span class="upper">Person Detected</span>: Du bist dafür, dass künstliche Intelligenzen neue Gesichter kennenlernen, möchtest aber deine Metadaten nicht in Verbindung mit deinem Gesicht weitergeben, dann nutze die <img src="static/img/detected.svg" alt="Detected" class="off-icon"> Filter. bezeichnet diejenigen Filter, die eine Person für die Software zwar erkennbar machen, jedoch keine Zuordnung der Metadaten ermöglichen. 
-					</p>
-					<p>
-						<img src="static/img/hidden.svg" alt="Hidden" class="off-icon"> <span class="upper">Person Hidden</span>: Die Gesichtserkennung hat deiner Meinung nach keine Vorteile und du möchtest dein Gesicht unerkennbar gestalten, dann sind es diese <img src="static/img/hidden.svg" alt="Hidden" class="off-icon"> Filter, welche du anwendest. Sie verunmöglichen die Erkennung eines Gesichts auf dem aufgenommenen Bild restlos.
-					</p>
-					<p>
-						Diese Webseite besitzt ein Gegenstück in Form eines gedruckten Readers. Dieser beinhaltet die insgesamt 544 verschiedenen Filter, sowie ausführliche Informationen zum Thema. Er bietet die Möglichkeit, sich auch ohne Zugang zum Internet zu informieren.
-					</p>
-					<p class="impressum">
-						Impressum: Bachelor Diplomarbeit 2019, ZHdK, Zürcher Hochschule der Künste<br>
-						Departement Design, Studienvertiefung Visuelle Kommunikation<br>
-						Autorin: Meret Fischli, Mentor/in: Cybu Richli, Martina Brassel<br>
-						© ZHdK Visuelle Kommunikation, Zürich, Juni 2019<br>
-						Interaction Design / Programmierung: Fernando Obieta<br>
-						Font: GT America Mono
-					</p>
-				</div>
+		<div class="main-wrapper grid-x grid-padding-x">
+			<div class="cell small-3">
+				<p>
+					Die Filter sind in folgende drei Kategorien eingeteilt:
+				</p>
+			</div>
+			<div class="cell small-3">
+				<p>
+					<img src="static/img/recognized.svg" alt="Recognized" class="off-icon"> <span class="upper">Person Recognized</span>: Bist du ein Befürworter/eine Befürworterin der Gesichtserkennung und willst dem OFF-Trend folgen, ist die Anwendung der <img src="static/img/recognized.svg" alt="Recognized" class="off-icon"> Filter die richtige Entscheidung. Dein Gesicht wird erkannt und hilft dabei, die Datenbanken der Gesichtserkennung mit deinen Metadaten, wie Name, Beruf, Wohnort,… zu erweitern. 
+				</p>
+				<p>
+					<img src="static/img/detected.svg" alt="Detected" class="off-icon"> <span class="upper">Person Detected</span>: Du bist dafür, dass künstliche Intelligenzen neue Gesichter kennenlernen, möchtest aber deine Metadaten nicht in Verbindung mit deinem Gesicht weitergeben, dann nutze die <img src="static/img/detected.svg" alt="Detected" class="off-icon"> Filter. bezeichnet diejenigen Filter, die eine Person für die Software zwar erkennbar machen, jedoch keine Zuordnung der Metadaten ermöglichen. 
+				</p>
+				<p>
+					<img src="static/img/hidden.svg" alt="Hidden" class="off-icon"> <span class="upper">Person Hidden</span>: Die Gesichtserkennung hat deiner Meinung nach keine Vorteile und du möchtest dein Gesicht unerkennbar gestalten, dann sind es diese <img src="static/img/hidden.svg" alt="Hidden" class="off-icon"> Filter, welche du anwendest. Sie verunmöglichen die Erkennung eines Gesichts auf dem aufgenommenen Bild restlos.
+				</p>
 			</div>
 		</div>
+
+		<div class="main-wrapper grid-x grid-padding-x">
+			<div class="cell small-6">
+				<p class="impressum">
+					Impressum: Bachelor Diplomarbeit 2019, ZHdK, Zürcher Hochschule der Künste<br>
+					Departement Design, Studienvertiefung Visuelle Kommunikation<br>
+					Autorin: Meret Fischli, Mentor/in: Cybu Richli, Martina Brassel<br>
+					© ZHdK Visuelle Kommunikation, Zürich, Juni 2019<br>
+					Interaction Design / Programmierung: Fernando Obieta<br>
+					Font: GT America Mono
+				</p>
+			</div>
+		</div>
+
+
 
 	</div>
 
